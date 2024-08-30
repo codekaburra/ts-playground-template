2302c70984b3a2e72e6ae70239df06762691c4cd
EM <eve.proj.fun@gmail.com>
2024-08-30 14:49:09 +0800

    init prettier

--- README.md
+++ README.md
@@ -1,16 +1,19 @@
 # ts-playground-template
 
 ## Initialize ts
+
 ```
 npx create-typescript-application
 ```
 
 ## Initialize prettier
+
 ```
 npm install --save-dev --save-exact prettier
 ```
 
 ## Initialize husky
+
 ```
 npm install --save-dev husky lint-staged
 npx husky init
--- test/index.spec.ts
+++ test/index.spec.ts
@@ -2,9 +2,9 @@
 import { expect } from 'chai';
 import { main } from '../src';
 
-describe('Index module', function() {
-  describe('expected behavior', function() {
-    it('should return hello world', function() {
+describe('Index module', function () {
+  describe('expected behavior', function () {
+    it('should return hello world', function () {
       expect(main()).to.equal('Hello World');
     });
   });
